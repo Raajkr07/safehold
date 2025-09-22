98269ba6c97ce0db4123aef1782588e6687bedeb
RAJ KUMAR <rk8210032@gmail.com>
2025-09-22 22:00:46 +0530

    added tabler icons dependency and mantine codes in tailwind config file

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -15,6 +15,15 @@ export default {
       colors: {
         'primary': '#53D347',
       },
+      keyframes: {
+        slideDown: {
+          '0%': { opacity: '0', transform: 'translateY(-10px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' }
+        }
+      },
+      animation: {
+        slideDown: 'slideDown 0.3s ease-out'
+      }
     },
   },
   plugins: [],
